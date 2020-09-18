fdc52fbc66b8b2ef49006131f8d0c28e12cb9d5c
saritazavala <saritazg2010@gmail.om>
2020-09-17 23:18:55 -0600

    YA ESTA LA INVERSA

--- ola.py
+++ ola.py
@@ -1,4 +1,7 @@
+from fractions import Fraction
+
 import numpy as np
+from past.builtins import xrange
 
 
 def zeros_matrix(rows, cols):
@@ -370,9 +373,25 @@ def getDeterminant(l):
         return (l[0][0]*l[1][1]-l[0][1]*l[1][0])
 
 
-A = [[1,3,5,9],[1,3,1,7],[4,3,9,7],[5,2,0,9]]  # Matrix from wiki
-t = transpose(A)
-print(getDeterminant(A))
-print(t)
+
+
+def getInverse(A):
+    ''' Returns the inverse of A, where  A is a square matrix in the form of a nested list of lists. '''
+    A = [A[i]+[int(i==j) for j in range(len(A))] for i in range(len(A))]
+    for i in range(len(A)):
+        A[i:] = sorted(A[i:], key=lambda r: -abs(r[i]))
+        A[i] = [A[i][j]/A[i][i] for j in range(len(A)*2)]
+        A = [[A[j][k] if i==j else A[j][k]-A[i][k]*A[j][i] for k in range(len(A)*2)] for j in range(len(A))]
+    return [A[i][-len(A):] for i in range(len(A))]
+
+A = [[1,2,3],[4,5,6],[7,2,9]]  # Matrix from wiki
+
+print(getInverse(A))
+# t = transpose(A)
+# print(getDeterminant(A))
+# print(t)
+
+
+
 
 
